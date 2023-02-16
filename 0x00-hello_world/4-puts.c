d5cc4e5b10e85793aee937ed5eddbd793582aab2
softwareSouf <software1souf@gmail.com>
2023-02-16 15:16:05 -0800

    update task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+
 #include <stdio.h>
 /**
 *main = Entry point 
@@ -8,5 +8,5 @@
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-                       return 0;
+                       return (0);
 }
