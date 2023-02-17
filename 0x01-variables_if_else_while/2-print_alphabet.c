a2ba01c4a00b8000f45e590b434b87956ee89de2
softwareSouf <software1souf@gmail.com>
2023-02-17 15:21:29 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main -  Entry point
+*description - incremant char with while function
+*
+*Return: 0
+*/
+int main(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
