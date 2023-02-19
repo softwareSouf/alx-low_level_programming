beff81ce6d61a8646c128faf4deb93f5cbed1583
softwareSouf <software1souf@gmail.com>
2023-02-19 05:17:54 -0800

    update task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
-#!/bin/bash
-
 #include <stdio.h>
-
+/**
+*main -  Entry point
+*description - the puts function print a statements
+*
+*Return: 0
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-		return 0;
-}		
+	return (0);
+}
