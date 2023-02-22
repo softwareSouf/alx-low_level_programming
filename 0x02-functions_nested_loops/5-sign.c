ddfe10c31554581d13efd19aa3e77ca3460cdfa4
softwareSouf <software1souf@gmail.com>
2023-02-22 07:32:42 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,9 @@
  * print_sign  - prints sign of x number
  * @n: the number that rely to the sign will prented
  *
- * Return: 0 if n equal 0 return 1 if n greater than 0 return -1 if n less than 0
+ * Return: 0 if n equal 0 return 1
+ * if n greater than 0
+ * return -1 if n less than 0
  */
 int print_sign(int n)
 {
@@ -15,7 +17,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar(0);
+		_putchar('0');
 		return (0);
 	}
 	else
