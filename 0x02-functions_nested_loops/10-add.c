d49f968a06b14b47a5bb58432c9cb2f566f710c5
softwareSouf <software1souf@gmail.com>
2023-02-22 13:49:34 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @x: the int number that will be added to y
+ * @y: the int number that woiill be added to x
+ * Return: the sum of x and y
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
