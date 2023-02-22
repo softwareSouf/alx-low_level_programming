5683ea9ce174a49bf19cd87d45e20ff661d71dea
softwareSouf <software1souf@gmail.com>
2023-02-22 08:34:20 -0800

    task 6 and 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @n: the int
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int la = n % 10;
+
+	if (la < 0)
+
+		la += -1;
+
+	_putchar(la + '0');
+	return (0);
+}
