e7c04e0aa0eeac486885b787c7fe3e7e32f830cd
softwareSouf <software1souf@gmail.com>
2023-03-14 08:11:38 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * strdup - duplicate string from pointer to an other
+ * @str: the string will duplicate
+ *
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *nst;
+	int i, r = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	nst = malloc((i + 1) * sizeof(str));
+	if (nst == NULL)
+	{
+		return (NULL);
+	}
+
+	for (r = 0; str[r]; r++)
+	{
+		str[r] = nst[r];
+	}
+	return (nst);
+}
