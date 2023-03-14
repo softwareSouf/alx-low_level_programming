c6d90d92070004f28d2e1b86b0db13b4f720dd2d
softwareSouf <software1souf@gmail.com>
2023-03-14 08:47:07 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,13 +15,11 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-		i = ci = 0;
-
+	i = ci = 0;
 	while (s1[i] != '\0')
 	{
 		i++;
@@ -31,19 +29,16 @@ char *str_concat(char *s1, char *s2)
 		ci++;
 	}
 	conct = malloc(sizeof(char) * (i + ci + 1));
-
 	if (conct == NULL)
 	{
 		return (NULL);
 	}
 	i = ci = 0;
-
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
 		i++;
 	}
-
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
