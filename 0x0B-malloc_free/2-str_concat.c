131bd9c9dc1419bc84cc2931e1e356e65854c168
softwareSouf <software1souf@gmail.com>
2023-03-14 08:52:14 -0700

    few changes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int i, ci;
+	int i, j;
 
 	if (s1 == NULL)
 	{
@@ -19,30 +19,30 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	i = ci = 0;
+	i = j  = 0;
 	while (s1[i] != '\0')
 	{
 		i++;
 	}
-	while (s2[ci] != '\0')
+	while (s2[j] != '\0')
 	{
-		ci++;
+		j++;
 	}
-	conct = malloc(sizeof(char) * (i + ci + 1));
+	conct = malloc(sizeof(char) * (i + j + 1));
 	if (conct == NULL)
 	{
 		return (NULL);
 	}
-	i = ci = 0;
+	i = j = 0;
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
 		i++;
 	}
-	while (s2[ci] != '\0')
+	while (s2[j] != '\0')
 	{
-		conct[i] = s2[ci];
-		i++, ci++;
+		conct[i] = s2[j];
+		i++, j++;
 	}
 	conct[i] = '\0';
 	return (conct);
