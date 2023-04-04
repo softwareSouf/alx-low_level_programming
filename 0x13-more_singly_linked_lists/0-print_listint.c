a696d704e9853c4687d074811be93b290cef0e54
softwareSouf <braha10bill@gmail.com>
2023-04-04 09:04:40 -0700

    task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
+
 /**
- * print_listint - prints all the elements
+ * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
@@ -15,6 +16,4 @@ size_t print_listint(const listint_t *h)
 		num++;
 		h = h->next;
 	}
-
-	return (num);
 }
