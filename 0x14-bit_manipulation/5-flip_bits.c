41731ab6be9fa59fab2e46274c1358fd27b8c8ef
softwareSouf <braha10bill@gmail.com>
2023-04-08 07:33:18 -0700

    tasks

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -15,7 +15,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (j = 63; j >= 0; j--)
 	{
-		current_bit = exclusive_bits >> i;
+		current_bit = exclusive_bits >> j;
 		if (current_bit & 1)
 			bit_count++;
 	}
