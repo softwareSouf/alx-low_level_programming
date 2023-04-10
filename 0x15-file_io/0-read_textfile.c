afeed3d069cc306296fb4866faa43884f14b1827
softwareSouf <braha10bill@gmail.com>
2023-04-10 07:21:42 -0700

    tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,18 +12,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
 	ssize_t fd;
-	ssize_t y;
-	ssize_t z;
+	ssize_t r;
+	ssize_t w;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	y = read(fd, buffer, letters);
-	z = write(STDOUT_FILENO, buffer, y);
+	r = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	free(buffer);
 	close(fd);
-	return (y);
+	return (r);
 }
