66606e8258e98f5c4071f78ddd79a8272a293ecf
softwareSouf <braha10bill@gmail.com>
2023-04-12 07:39:38 -0700

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
+	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
 	o = open(filename, O_RDONLY);
